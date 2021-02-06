e7a29131384755440afc22bee1175b9c9d7fe56b
mulloa <mulloa@LTP-PCN-05>
2021-02-05 23:59:38 -0400

    Completed course

---
+++ src/app.js
@@ -0,0 +1,60 @@
+document.getElementById('formTask').addEventListener('submit', SaveTask)
+GetTasks();
+
+function SaveTask(e){
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
+    
+    const task = {
+        title,
+        description
+    };
+
+    if(localStorage.getItem('tasks') === null) {
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
+
+      GetTasks();
+      document.getElementById('formTask').reset();
+
+    e.preventDefault();
+}
+
+function GetTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+
+    tasksView.innerHTML = '';
+
+    for(let i = 0; i < tasks.length; i++) {
+
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+
+      tasksView.innerHTML += `<div class="card mb-3">
+      <div class="card-body">
+      <p>${title} - ${description}</p>
+      <a class="btn btn-danger" onclick="DeleteTask('${title}')">Eliminar</a>
+      </div>
+      </div>`
+    }
+  }
+  
+function DeleteTask(title){
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    for(let i =0; i < tasks.length; i++){
+        if(tasks[i].title == title){
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    GetTasks();
+ }
